9b8e539eba6df5bce3c89894e041ad1590f26f32
Messyginger0804 <j.c.ashley4363@gmail.com>
2023-11-14 10:47:39 -0600

    set up mongo db and .env file

---
+++ src/test/java/app/braylees/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package app.braylees.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
